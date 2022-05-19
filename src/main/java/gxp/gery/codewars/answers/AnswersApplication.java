4ca5e529e5a7260b509674eecdd3ceed51bd355b
Geryenko Hawsen <gery.zarons@gmail.com>
2022-05-19 10:43:43 +0900

    build: add starter code

---
+++ src/main/java/gxp/gery/codewars/answers/AnswersApplication.java
@@ -0,0 +1,13 @@
+package gxp.gery.codewars.answers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnswersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnswersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gxp/gery/codewars/answers/AnswersApplicationTests.java
@@ -0,0 +1,13 @@
+package gxp.gery.codewars.answers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnswersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
