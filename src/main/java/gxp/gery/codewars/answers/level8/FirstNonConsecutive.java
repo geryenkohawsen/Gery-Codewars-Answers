4cfe3277dd66f3c8f12e998502615c1bf272c35f
Geryenko Hawsen <gery.zarons@gmail.com>
2022-05-20 17:45:36 +0900

    feat: add answer for 8 kyu Find the first non-consecutive number

---
+++ src/main/java/gxp/gery/codewars/answers/level8/FirstNonConsecutive.java
@@ -0,0 +1,21 @@
+package gxp.gery.codewars.answers.level8;
+
+class FirstNonConsecutive {
+    static Integer find(final int[] array) {
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i + 1] - array[i] != 1) {
+                return array[i + 1];
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(FirstNonConsecutive.find(new int[]{1, 2, 3, 4, 6, 7, 8})); //6
+        System.out.println(FirstNonConsecutive.find(new int[]{1, 2, 3, 4, 5, 6, 7, 8})); //null
+        System.out.println(FirstNonConsecutive.find(new int[]{4, 6, 7, 8, 9, 11})); //6
+        System.out.println(FirstNonConsecutive.find(new int[]{4, 5, 6, 7, 8, 9, 11})); //11
+        System.out.println(FirstNonConsecutive.find(new int[]{-3, -2, 0, 1})); //-0
+    }
+}
