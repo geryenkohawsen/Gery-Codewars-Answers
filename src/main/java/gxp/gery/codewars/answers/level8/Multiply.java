38085d0221b4a338a155f366270795825b67c748
Geryenko Hawsen <gery.zarons@gmail.com>
2022-05-21 13:17:52 +0900

    feat: add answer for 8 kyu Multiply

---
+++ src/main/java/gxp/gery/codewars/answers/level8/Multiply.java
@@ -0,0 +1,13 @@
+package gxp.gery.codewars.answers.level8;
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Multiply.multiply(2.0, 5.0));
+        System.out.println(Multiply.multiply(3.0, 3.0));
+        System.out.println(Multiply.multiply(3.333, 12.123));
+    }
+}
