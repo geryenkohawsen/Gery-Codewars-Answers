52bf53e8c52adcf70a2c327cd2246ac2f2d2b11b
Geryenko Hawsen <gery.zarons@gmail.com>
2022-05-23 13:17:31 +0900

    test: add test class for 8 kyu Multiply

---
+++ src/test/java/gxp/gery/codewars/answers/level8/MultiplyTest.java
@@ -0,0 +1,17 @@
+package gxp.gery.codewars.answers.level8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MultiplyTest {
+
+    @Test
+    void testSomething() {
+        assertEquals(1, Multiply.multiply(1.0, 1.0));
+        assertEquals(4, Multiply.multiply(2.0, 2.0));
+        assertEquals(4, Multiply.multiply(1.0, 4.0));
+        assertEquals(4, Multiply.multiply(-2.0, -2.0));
+        assertEquals(94.39691643, Multiply.multiply(7.123, 13.25241));
+    }
+}
