9d51dc945dad19c21aa584b57ecf505f8a4df3f5
Geryenko Hawsen <gery.zarons@gmail.com>
2022-05-26 16:31:18 +0900

    feat: add answer for 8 kyu Fake Binary

---
+++ src/main/java/gxp/gery/codewars/answers/level8/FakeBinary.java
@@ -0,0 +1,8 @@
+package gxp.gery.codewars.answers.level8;
+
+public class FakeBinary {
+
+    public static String fakeBin(String numberString) {
+       return numberString.replaceAll("[0-4]", "0").replaceAll("[5-9]", "1");
+    }
+}
