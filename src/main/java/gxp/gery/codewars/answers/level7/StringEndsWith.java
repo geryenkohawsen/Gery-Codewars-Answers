d31f02506dbd15c53eb5b01e057b62a4abb1b7a2
Geryenko Hawsen <gery.zarons@gmail.com>
2022-06-01 17:05:15 +0900

    feat: add answer for 7 kyu String ends with?

---
+++ src/main/java/gxp/gery/codewars/answers/level7/StringEndsWith.java
@@ -0,0 +1,8 @@
+package gxp.gery.codewars.answers.level7;
+
+public class StringEndsWith {
+
+    public static boolean stringEndsWith(String str, String ending) {
+        return str.endsWith(ending);
+    }
+}
