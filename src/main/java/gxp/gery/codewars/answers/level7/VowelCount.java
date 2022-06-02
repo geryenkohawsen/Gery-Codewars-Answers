4286b0c8138f6e90e9f79871fff98bb0164032e4
Geryenko Hawsen <gery.zarons@gmail.com>
2022-06-02 10:43:32 +0900

    feat: add answer for 8 kyu Vowel Count

--- src/main/java/gxp/gery/codewars/answers/level7/VowelCount.java
+++ src/main/java/gxp/gery/codewars/answers/level7/VowelCount.java
@@ -2,14 +2,7 @@ package gxp.gery.codewars.answers.level7;
 
 public class VowelCount {
 
-    public static int countVowel(String str) {
-        return str.replaceAll("(?i)[^aiueo]", "").length();
-    }
-
-    public static void main(String[] args) {
-        System.out.println(VowelCount.countVowel("aiueo")); //5
-        System.out.println(VowelCount.countVowel("aiUeO")); //5
-        System.out.println(VowelCount.countVowel("abracadabra")); //5
-        System.out.println(VowelCount.countVowel("BUy 100 SaUsAge")); //5
+    public static String countVowel(String str) {
+        return String.valueOf(str.replaceAll("(?i)[^aiueo]", "").length());
     }
 }
