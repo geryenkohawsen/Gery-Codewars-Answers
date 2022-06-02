91ebddd7c4e3b9704eb5b66688df40ff5188b632
Geryenko Hawsen <gery.zarons@gmail.com>
2022-06-02 11:03:29 +0900

    feat: add answer for 8 kyu Returning Strings

---
+++ src/main/java/gxp/gery/codewars/answers/level8/ReturningStrings.java
@@ -0,0 +1,8 @@
+package gxp.gery.codewars.answers.level8;
+
+public class ReturningStrings {
+
+    public static String greet(String name) {
+        return "Hello, " + name + " how are you doing today?";
+    }
+}
