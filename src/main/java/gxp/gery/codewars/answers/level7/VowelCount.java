6cfc0507723fce8c113fbbf3ebdbbd20d9cbf6c4
Geryenko Hawsen <gery.zarons@gmail.com>
2022-06-02 10:48:09 +0900

    feat: update answer for 7 kyu Vowel Count

--- src/main/java/gxp/gery/codewars/answers/level7/VowelCount.java
+++ src/main/java/gxp/gery/codewars/answers/level7/VowelCount.java
@@ -2,7 +2,7 @@ package gxp.gery.codewars.answers.level7;
 
 public class VowelCount {
 
-    public static String countVowel(String str) {
-        return String.valueOf(str.replaceAll("(?i)[^aiueo]", "").length());
+    public static int countVowel(String str) {
+        return str.replaceAll("(?i)[^aiueo]", "").length();
     }
 }
