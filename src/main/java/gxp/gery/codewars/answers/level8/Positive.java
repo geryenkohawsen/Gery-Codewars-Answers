12217c8a5c83f113eb1720521d3c0a76a21e6463
Geryenko Hawsen <gery.zarons@gmail.com>
2022-06-03 14:52:46 +0900

    fix: fix answer for 8 kyu Sum of positive

--- src/main/java/gxp/gery/codewars/answers/level8/Positive.java
+++ src/main/java/gxp/gery/codewars/answers/level8/Positive.java
@@ -2,4 +2,13 @@ package gxp.gery.codewars.answers.level8;
 
 public class Positive {
 
+    public static int sum(int[] arr) {
+        int result = 0;
+
+        for (int j : arr) {
+            result += Math.max(j, 0);
+        }
+
+        return result;
+    }
 }
