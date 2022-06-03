7e8773b764314e30cae4947bfcecda7a0ce0cc4b
Geryenko Hawsen <gery.zarons@gmail.com>
2022-06-03 15:37:57 +0900

    feat: add answer for 8 kyu Reversed Strings

---
+++ src/main/java/gxp/gery/codewars/answers/level8/ReversedStrings.java
@@ -0,0 +1,10 @@
+package gxp.gery.codewars.answers.level8;
+
+public class ReversedStrings {
+
+    public static String reverseStrings(String str) {
+
+        StringBuilder input1 = new StringBuilder();
+        return String.valueOf(input1.append(str).reverse());
+    }
+}
